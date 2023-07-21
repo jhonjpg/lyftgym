b2ec535a9cda0f17b789f4c5b53362e2ad1f287d
jhonjpg <jonathan050315jj@gmail.com>
2023-07-21 11:29:18 -0400

    compl

--- src/components/FrontPg.jsx
+++ src/components/FrontPg.jsx
@@ -11,6 +11,10 @@ const FrontPg = () => {
 <section id="journey">
 
 <div className="frontimgPage">
+
+<div className="containerImg"> <img src="img/gymFront.jpg" alt="" /></div>
+
+
 <div className="header"> 
 <h3 className="z-3">Empieza tu Entrenamiento </h3> <span className="z-3">Hoy</span> <p className="z-3"> Ven unete a nosotros   y empieza a start mas cerca de tu cuerpo deseado</p>
 
